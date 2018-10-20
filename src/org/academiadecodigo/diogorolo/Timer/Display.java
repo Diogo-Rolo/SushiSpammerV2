f85b8095b3c1f8b09ffe77321fc3d7f28083b021
MasterBossMan <coiso60@gmail.com>
2018-10-20 17:31:22 +0100

    Added class Display

---
+++ src/org/academiadecodigo/diogorolo/Timer/Display.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.diogorolo.Timer;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Display {
+
+    private int size = 30;
+    private int higth;
+    private int width;
+    private int padding = 40;
+
+    private Rectangle background;
+    private Rectangle n1;
+    private Rectangle n2;
+
+    public Display(){
+
+        
+    }
+
+}
